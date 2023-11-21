bf3f9d6be8317ec9dfbad71051f63ca3e642ecc7
Flame <5017_30@mail.ru>
2023-11-21 21:24:33 +0300

    add: article entity

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+
+import { Repository } from "typeorm";
+import { ArticleEntity } from "./entities/article.entity";
 
 @Injectable()
 export class ArticleService {
-  async create() {
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly userRepository: Repository<ArticleEntity>,
+  ) {}
+  async createArticle() {
+    console.log(this.userRepository);
     return "create";
   }
 }
